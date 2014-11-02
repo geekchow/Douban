d35a00216e30b162ac963187c581f34582d2366c
wizchentong <chentong@wiz.cn>
2014-11-02 18:08:02 +0800

    init

---
+++ app/src/main/java/com/czt/douban/util/PackageInfoUtil.java
@@ -0,0 +1,39 @@
+package com.czt.douban.util;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * Created by GavinCT on 2014/10/28.
+ */
+public class PackageInfoUtil {
+    public static Integer getVersionCode(Context context){
+        PackageInfo info = getPackageInfo(context);
+        if(info == null){
+            return null;
+        }
+        return info.versionCode;
+    }
+    public static String getVersionName(Context context){
+        PackageInfo info = getPackageInfo(context);
+        if(info == null){
+            return null;
+        }
+        return info.versionName;
+    }
+
+    private static PackageInfo getPackageInfo(Context context) {
+        try {
+            PackageManager manager = context.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            return info;
+        } catch (PackageManager.NameNotFoundException e) {
+            //can't reach
+            e.printStackTrace();
+        }
+        //can't reach
+        return null;
+    }
+}
